22eb7e7b82a16dac36844948c07869f6158f2005
Lucky Dubey <80376915+luckydubey10@users.noreply.github.com>
2024-11-28 15:19:11 +0530

    Add files via upload

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,3 @@
 export const LOGO_URL = "https://femina.wwmindia.com/thumb/content/2021/nov/healthy-011636373981.jpg?width=1200&height=900";
-export const main_logo = "./main-logo.jpg"
+export const main_logo = "./main-logo.jpg",
+REACT_APP_API_URL= "https://www.swiggy.com"
