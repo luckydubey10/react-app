2e0358c15ed228572ffaeb69ab776a7cc3230bd8
Lucky Dubey <80376915+luckydubey10@users.noreply.github.com>
2024-11-28 15:18:09 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import HeaderComponent from './components/Header';
 import BodyComponent from './components/Body';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+// import AboutComponent from './components/About';
+import ContactComponent from './components/Contact';
+import ErrorComponent from './components/Error';
+import RestaurantMenu from './components/RestaurantMenu';
+
+const AboutComponent = lazy(()=> import('./components/About'))
 
 /**
  * Header
@@ -19,10 +26,37 @@ const AppComponent = () =>{
     return (
     <div className='appLayout'>
         <HeaderComponent/>
-        <BodyComponent/>
+        {/** */}
+        <Outlet/>
     </div>
     )}
 
+const appRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppComponent/>,
+        errorElement: <ErrorComponent/>,
+        children:[
+            {
+                path: '/',
+                element: <BodyComponent/>
+            },
+            {
+                path: 'about',
+                element: <Suspense fallback={'Loading...'}><AboutComponent/></Suspense>
+            },
+            {
+                path: 'contact',
+                element: <ContactComponent/>
+            },
+            {
+                path: 'restaurant/:resId',
+                element: <RestaurantMenu/>
+            }
+        ]
+    }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<AppComponent/>);
+root.render(<RouterProvider router={appRouter}/>);
